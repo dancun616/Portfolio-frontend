acbea35e6602d58eb1b18180844c723d05e197c5
dancun616 <dancun616@gmail.com>
2023-03-06 18:49:30 +0300

    fixed-ui

--- src/components/AboutContent.js
+++ src/components/AboutContent.js
@@ -14,16 +14,7 @@ const AboutContent = () => {
         <button className="btn">Contact</button>
       </Link>
     </div>
-    <div className="right">
-      <div className="img-container">
-        <div className="img-stack top">
-          <img src={React1} className="img" alt="true"/>
-        </div>
-        <div className="img-stack bottom">
-          <img src={React2} className="img" alt="true"/>
-        </div>
-      </div>
-    </div>
+
   </div>;
 };
 
